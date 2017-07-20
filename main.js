a9a4a2132e6602db7c56d780cac8e8dda6bbbcbf
Inbal Langer <inbalanger@gmail.com>
2017-07-20 07:49:34 +0300

    more css

--- main.js
+++ main.js
@@ -1,52 +1,53 @@
-const stockSymbbole = [
-  "WIX",
-  "MSFT",
-  "YHOO"
-];
+(function () {
+  const stockSymbbole = [
+    "WIX",
+    "MSFT",
+    "YHOO"
+  ];
 
-const stockData = [
-  {
-    "Symbol": "WIX",
-    "Name": "Wix.com Ltd.",
-    "Change": "0.750000",
-    "PercentChange": "+1.51%",
-    "LastTradePriceOnly": "76.099998"
-  },
-  {
-    "Symbol": "MSFT",
-    "Name": "Microsoft Corporation",
-    "PercentChange": "-2.09%",
-    "Change": "-0.850006",
-    "LastTradePriceOnly": "69.620003"
-  },
-  {
-    "Symbol": "YHOO",
-    "Name": "Yahoo! Inc.",
-    "Change": "0.279999",
-    "PercentChange": "+1.11%",
-    "LastTradePriceOnly": "50.599998"
+  const stockData = [
+    {
+      "Symbol": "WIX",
+      "Name": "Wix.com Ltd.",
+      "Change": "0.750000",
+      "PercentChange": "+1.51%",
+      "LastTradePriceOnly": "76.099998"
+    },
+    {
+      "Symbol": "MSFT",
+      "Name": "Microsoft Corporation",
+      "PercentChange": "-2.09%",
+      "Change": "-0.850006",
+      "LastTradePriceOnly": "69.620003"
+    },
+    {
+      "Symbol": "YHOO",
+      "Name": "Yahoo! Inc.",
+      "Change": "0.279999",
+      "PercentChange": "+1.11%",
+      "LastTradePriceOnly": "50.599998"
+    }
+  ];
+
+  function generateStockList(stockDataList) {
+    let stockListElm = stockDataList.map(function (stock) {
+      return `<li id="${stock.Symbol}">${createStock(stock)}</li>`
+
+    });
+
+    stockListElm = stockListElm.join('');
+    console.log(stockListElm);
+    return `<ul class="reset-list stock-list">${stockListElm}</ul>`;
   }
-];
 
-function generateStockList(stockDataList) {
-  let stockListElm = stockDataList.map(function (stock) {
-    return `<li>${createStock(stock)}</li>`
-
-  });
-
-  stockListElm = stockListElm.join('');
-  console.log(stockListElm);
-  return `<ul class="reset-list stock-list">${stockListElm}</ul>`;
-}
-
-function createStock(stockData) {
-  return `
+  function createStock(stockData) {
+    return `
       <span class="stock-name">
         <span>${stockData.Symbol}</span>
         <span> (${stockData.Name})</span>
       </span>
       <div class="stock-info">
-        <span>${stockData.LastTradePriceOnly}</span>
+        <span class="stock-data">${stockData.LastTradePriceOnly}</span>
         <button class="${buttonColor(stockData)}">${stockData.PercentChange}</button>
         <span class="upDown">
           <button class="up"><img src="assets/svg/arrow.svg"></button>
@@ -54,17 +55,19 @@ function createStock(stockData) {
         </span>
        </div>
    `;
-}
-function buttonColor(stockData){
-  if(stockData.Change >= 0){
-    return 'green';
   }
-  else{
-    return 'red';
+
+  function buttonColor(stockData) {
+    if (stockData.Change >= 0) {
+      return 'green';
+    }
+    else {
+      return 'red';
+    }
   }
-}
-function createHeader() {
-  return `<header>
+
+  function createHeader() {
+    return `<header>
     <h1>STOKR</h1>
     <ul class="menu reset-list">
       <li>
@@ -81,14 +84,20 @@ function createHeader() {
       </li>
     </ul>
   </header>`;
-}
-function createMain(stockData) {
-  return `<main>${generateStockList(stockData)}</main>`;
-}
+  }
 
-function init(stockData) {
-  return `${createHeader()}
+  function createMain(stockData) {
+    return `<main>${generateStockList(stockData)}</main>`;
+  }
+
+  function init(stockData) {
+    return `${createHeader()}
           ${createMain(stockData)}`;
-}
-const divElm = document.querySelector('.stock-list-page');
-divElm.innerHTML = init(stockData);
+  }
+  function addListnerEvent() {
+    
+  }
+  const divElm = document.querySelector('.stock-list-page');
+  divElm.innerHTML = init(stockData);
+
+}())
